5d7efbbb5a4bdd1197207a529bc5e8de1e740b68
kliperd <kliperd@naver.com>
2022-12-27 16:33:11 +0900

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,77 +1,105 @@
-process.env.NODE_ENV = 'test';
+var rawbody = require('raw-body');
 
-var request = require('supertest');
-var xpect = require('chai').expect;
-var express = require('express');
-var app = express();
-var sqlinjection = require('../lib/index');
+function hasSql(value) {
+   
 
-describe('basic tests', function() {
+    if (value === null || value === undefined) {
+        //console.log('inject error => value unefined');
+        return false;
+    }
 
-    it('returns function', function(done) {
+    // sql regex reference: http://www.symantec.com/connect/articles/detection-sql-injection-and-cross-site-scripting-attacks
+    var sql_meta = new RegExp('(%27)|(\')|(--)|(%23)|(#)', 'i');
+    if (sql_meta.test(value)) {
+        return true;
+    }
 
-        xpect(sqlinjection).to.be.a('function');
-        done();
-    });
-});
+    var sql_meta2 = new RegExp('((%3D)|(=))[^\n]*((%27)|(\')|(--)|(%3B)|(;))', 'i');
+    if (sql_meta2.test(value)) {
+       // console.log('inject error => sql_meta2');
+        return true;
+    }
 
-describe('supertests', function() {
+    var sql_typical = new RegExp('w*((%27)|(\'))((%6F)|o|(%4F))((%72)|r|(%52))', 'i');
+    if (sql_typical.test(value)) {
+       // console.log('inject error => sql_typical');
+        return true;
+    }
 
-    before(function(done) {
+    var sql_union = new RegExp('((%27)|(\'))union', 'i');
+    if (sql_union.test(value)) {
+       // console.log('inject error => sql_union');
+        return true;
+    }
 
-        app.use(sqlinjection);
-        app.get('/test', function(req, res) {
-            res.status(200).send({});
+    return false;
+}
+
+function middleware(req, res, next) {
+
+    var containsSql = false;
+
+    //console.log(111 , req.originalUrl , hasSql(req.originalUrl));
+    if (req.originalUrl !== null && req.originalUrl !== undefined) {
+        if (hasSql(req.originalUrl) === true) {
+            containsSql = true;
+        }
+    }
+
+    //console.log(222 , req.originalUrl , containsSql, req.method);
+
+    if (containsSql === false) {
+        rawbody(req, {
+            encoding: 'utf8'
+        }, function(err, body) {
+
+            //console.log('222', err,body);
+            if (err) {
+                return next(err);
+            }
+
+           
+
+            if (body !== null && body !== undefined) {
+
+                if (typeof body !== 'string') {
+                    console.log('inject error => body-string');
+                    body = JSON.stringify(body);
+                }
+
+               // console.log(333, body , containsSql);
+               // console.log(hasSql(body));
+
+                if (hasSql(body) === true) {
+                    console.log('inject error => hassql_body');
+                    containsSql = true;
+                }
+              //  console.log(444 ,containsSql);
+            }
+
+            if (containsSql === true) {
+                res.status(403).json({
+                    //error: 'SQL Detected in Request, Rejected.'
+                    error: 'Rejected. 1099922'
+                });
+            } else {
+               // console.log(555);
+                next();
+            }
         });
-        app.get('/users/:uid', function(req, res) {
-            res.status(200).send({});
+
+       // console.log(666, containsSql, req.method);
+       
+        //modify 1227
+        if(req.method != 'GET'){
+            next();
+        }
+    } else {
+        res.status(403).json({
+            //error: 'SQL Detected in Request, Rejected.'
+            error: 'Rejected. 1099922'
         });
-        app.post('/body', function(req, res) {
-            res.status(200).send({});
-        });
-        done();
-    });
+    }
+}
 
-    after(function(done) {
-        done();
-    });
-
-    it('can call app', function(done) {
-
-        request(app).get('/test').expect(200, done);
-    });
-
-    it('querystring no sql', function(done) {
-
-        request(app).get('/test?attack=noattack').expect(200, done);
-    });
-
-    it('param no sql', function(done) {
-
-        request(app).get('/users/1').expect(200, done);
-    });
-
-    it('body no sql', function(done) {
-
-        request(app).post('/body').send({
-            stuff: 'somestuff'
-        }).expect(200, done);
-    });
-
-    it('querystring with sql', function(done) {
-
-        request(app).get('/test?attack=' + escape('WHERE field = \'anything\' OR \'x\'=\'x\'\'')).expect(403, done);
-    });
-
-    it('param with sql', function(done) {
-
-        request(app).get('/users/' + escape('WHERE field = \'anything\' OR \'x\'=\'x\'\'')).expect(403, done);
-    });
-
-    it('body with sql', function(done) {
-
-        request(app).post('/body').send({
-            stuff: 'WHERE field = \'anything\' OR \'x\'=\'x\'\''
-        }).expect(403, done);
-    });
-});
+module.exports = middleware;
